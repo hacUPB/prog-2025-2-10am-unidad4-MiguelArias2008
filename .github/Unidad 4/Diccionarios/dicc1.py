4c00646f4d1e94fb939dcc016825ca6def987195
Miguel Arias <miguelangel.arias@upb.edu.co>
2025-10-02 11:31:23 -0500

    diccionarios

---
+++ .github/Unidad 4/Diccionarios/dicc1.py
@@ -0,0 +1,32 @@
+# Diccionario vacío
+aeronave = {}
+
+# Diccionario con elementos
+aeronave = {
+    "modelo": "Boeing 787-9",
+    "envergadura": 60.17,  # metros
+    "longitud": 62.81,     # metros
+    "mtow": 254000,        # kg
+    "velocidad_max": 954   # km/h
+}
+
+aeronave ["altura"] = 16.9
+
+# Diccionario con diferentes tipos de datos como valores
+vuelo = {
+    "numero": "AA123",
+    "origen": "KLAX",
+    "destino": "KJFK",
+    "distancia": 3983,
+    "a_tiempo": True,
+    "tripulacion": ["Capitán Smith", "F/O Johnson", "F/E Williams"]
+}
+print(f"Origen es {vuelo["origen"]}")
+print(f"Destino es {vuelo["destino"]}")
+for i in vuelo["tripulacion"]:
+    print(i)
+
+
+# Creación con dict()
+motor = dict(fabricante="GE", modelo="GE9X", empuje=470, bypass_ratio=10)
+
